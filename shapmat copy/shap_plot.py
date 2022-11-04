20b4686a8c50d8e36034305a54d73577f2f9dac5
Ryza <ryzary25@gmail.com>
2022-11-04 13:59:33 +0900

    add shapmat/copy to git_ignore

--- shapmat copy/shap_plot.py
+++
@@ -1,8 +0,0 @@
-import shap
-
-def summary_plot(shap_values,X,max_display=15):
-    shap.summary_plot(shap_values,X,show=False,plot_size=None, max_display=max_display)
-
-def waterfall_plot(explainer,shap_df,subject_id,max_display=10):
-    shap.plots._waterfall.waterfall_legacy(explainer.expected_value[1], shap_df.loc[subject_id],
-     show=False, features=shap_df.loc[subject_id], max_display=max_display)

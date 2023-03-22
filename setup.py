daacc75b025d4e8e38eedd328b58fd88426308d7
ryzary <ryzary25@gmail.com>
2023-03-22 14:14:59 +0900

    v0.1.3

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
 setup(
     name="shapmat",
     packages=["shapmat"],
-    version="0.1.2",
+    version="0.1.3",
     description="SHAP-based Microbiome Analyses Tool",
     long_description=long_description,
     long_description_content_type="text/markdown",
